198737b3e2903d0b573bb5495caba332d863dbf5
lyc <32991926+yc0037@users.noreply.github.com>
2020-07-28 18:25:56 +0800

    update page

---
+++ examples/bdmt-visual/tb/precache-manifest.94d3dd906fe14c5d49a86bf8eedf1b1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eda55b65847a682a6f520a045e9ffb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1fcbfdca5d79ed1aa141",
+    "url": "./static/css/2.e7180fea.chunk.css"
+  },
+  {
+    "revision": "854d0a54c08f3d9751b8",
+    "url": "./static/css/main.7512d0d3.chunk.css"
+  },
+  {
+    "revision": "1fcbfdca5d79ed1aa141",
+    "url": "./static/js/2.57e20f41.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.57e20f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854d0a54c08f3d9751b8",
+    "url": "./static/js/main.6238920b.chunk.js"
+  },
+  {
+    "revision": "20aae88910fc1a4c49d2",
+    "url": "./static/js/runtime-main.e25272e9.js"
+  }
+]);
--- examples/bdmt-visual/tb/service-worker.js
+++ examples/bdmt-visual/tb/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8dc10c7b00cbbb3d3e4517228730eb9d.js"
+  "./precache-manifest.94d3dd906fe14c5d49a86bf8eedf1b1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
